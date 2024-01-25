05bb71bfee5410ebe198f67cb2da5fe52e2beb8b
HaileyEmeline <hschoppe@uvm.edu>
2024-01-25 16:33:49 -0500

    Initial creation of files

---
+++ header.h
@@ -0,0 +1,8 @@
+//
+// Created by darkf on 1/25/2024.
+//
+
+#ifndef M1OEP_HSCHOPPE_HEADER_H
+#define M1OEP_HSCHOPPE_HEADER_H
+
+#endif //M1OEP_HSCHOPPE_HEADER_H
---
+++ main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by darkf on 1/25/2024.
+//
